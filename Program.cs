0be272d025254a53c742d14764e444d034983ae8
Connor ORourke <orourkeconnor@hotmail.co.uk>
2023-05-05 12:36:42 +0100

    allow the player to input an attack or heal option with relevant output to the console

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int playerHealth = 30;
+int enemyHealth = 20;
+
+int playerAttack = 5;
+int enemyAttack = 7;
+
+int healAmount = 5;
+
+while(playerHealth > 0 && enemyHealth > 0)
+{
+    Console.WriteLine("-- Player Turn --");
+    Console.WriteLine("Enter 'a' to attack or 'h' to heal.");
+
+    string choice = Console.ReadLine();
+
+    if(choice == "a")
+    {
+        enemyHealth -= playerAttack;
+        Console.WriteLine($"Player attacks the enemy and deals {playerAttack} damage");
+    }
+    else {
+        playerHealth += healAmount;
+        Console.WriteLine($"Player has healed by {healAmount} health");
+    }
+}
