207dc526fe455a524086da7d13612f9a45caee5b
Connor ORourke <orourkeconnor@hotmail.co.uk>
2023-05-05 14:10:03 +0100

    add an enemy that uses random inputs to either attack or heal

--- Program.cs
+++ Program.cs
@@ -6,9 +6,12 @@ int enemyAttack = 7;
 
 int healAmount = 5;
 
+Random random = new Random();
+
 while(playerHealth > 0 && enemyHealth > 0)
 {
     Console.WriteLine("-- Player Turn --");
+    Console.WriteLine($"Player health: {playerHealth} Enemy Health: {enemyHealth}");
     Console.WriteLine("Enter 'a' to attack or 'h' to heal.");
 
     string choice = Console.ReadLine();
@@ -21,5 +24,24 @@ while(playerHealth > 0 && enemyHealth > 0)
     else {
         playerHealth += healAmount;
         Console.WriteLine($"Player has healed by {healAmount} health");
+    }
+
+    if(enemyHealth > 0)
+    {
+        Console.WriteLine("-- Enemy Turn --");
+        Console.WriteLine($"Player health: {playerHealth} Enemy Health: {enemyHealth}");
+
+        int enemyChoice = random.Next(0, 2);
+
+        if(enemyChoice == 0)
+        {
+            playerHealth -= enemyAttack;
+            Console.WriteLine($"Enemy attacks the player and deals {enemyAttack} damage");
+        }
+        else 
+        {
+            enemyHealth += healAmount;
+            Console.WriteLine($"Enemy has healed by {healAmount} health");
+        }
     }
 }
